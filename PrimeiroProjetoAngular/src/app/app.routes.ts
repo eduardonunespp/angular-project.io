98e86e07e5e9f733b523e0bb3999319381877db8
Carlos Eduardo <carlospn200406@gmail.com>
2023-03-03 20:58:35 -0300

    feat: add routes produto-detalhe and carrinho

--- PrimeiroProjetoAngular/src/app/app.routes.ts
+++ PrimeiroProjetoAngular/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { ContatoComponent } from "./institucional/contato/contato.component";
 import { SobreComponent } from "./institucional/sobre/sobre.component";
 import { DataBindingComponent } from "./demos/data-binding/data-binding.component";
 import { ListaProdutoComponent } from "./produtos/lista-produto/lista-produto.component";
+import { Component } from "@angular/core";
 
 export const rootRouterConfig: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -11,5 +12,7 @@ export const rootRouterConfig: Routes = [
     { path: 'contato', component: ContatoComponent },
     { path: 'sobre', component: SobreComponent },
     { path: 'feature-data-binding', component: DataBindingComponent },
-    { path: 'produtos', component: ListaProdutoComponent}
+    { path: 'produtos', component: ListaProdutoComponent},
+    { path: 'produto-detalhe/:id', component: ListaProdutoComponent},
+    { path: 'carrinho/:id', component: ListaProdutoComponent}
 ];
