820df6cbfe7c22517dd3e2e010d3c9fb902e3ba8
Carlos Eduardo <carlospn200406@gmail.com>
2023-03-11 17:07:15 -0300

    refactor: add navegacao.module.ts

--- PrimeiroProjetoAngular/src/app/app.module.ts
+++ PrimeiroProjetoAngular/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, asNativeElements } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router'
 import { APP_BASE_HREF } from '@angular/common';
@@ -13,9 +13,6 @@ import { TextMaskModule } from 'angular2-text-mask';
 import { CustomFormsModule } from 'ng2-validation';
 
 import { AppComponent } from './app.component';
-import { MenuComponent } from './navegacao/menu/menu.component';
-import { HomeComponent } from './navegacao/home/home.component';
-import { FooterComponent } from './navegacao/footer/footer.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
 import { rootRouterConfig } from './app.routes';
@@ -23,13 +20,11 @@ import { DataBindingComponent } from './demos/data-binding/data-binding.componen
 import { ProdutosService } from './produtos/produtos.service';
 import { ListaProdutoComponent } from './produtos/lista-produto/lista-produto.component';
 import { CadastroComponent } from './demos/reactForms/cadastro/cadastro.component';
+import { NavegacaoModule } from './navegacao/navegacao.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
-    HomeComponent,
-    FooterComponent,
     SobreComponent,
     ContatoComponent,
     DataBindingComponent,
@@ -37,6 +32,7 @@ import { CadastroComponent } from './demos/reactForms/cadastro/cadastro.componen
     CadastroComponent
   ],
   imports: [
+    NavegacaoModule,
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
