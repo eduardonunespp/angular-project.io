bad8ce1cc7c7d00c65a3b632d63f10a97d1ffe9b
Carlos Eduardo <carlospn200406@gmail.com>
2023-03-13 21:15:27 -0300

    feat: add produto-dashboard structure

--- PrimeiroProjetoAngular/src/app/demos/arquitetura-componentes/produto-dashboard/produto-dashboard.component.ts
+++ PrimeiroProjetoAngular/src/app/demos/arquitetura-componentes/produto-dashboard/produto-dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Produto } from '../models/produto';
 
 @Component({
   selector: 'app-produto-dashboard',
@@ -6,6 +7,58 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class ProdutoDashboardComponent {
+export class ProdutoDashboardComponent implements OnInit {
+
+  produtos: Produto[]
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.produtos = [
+      {
+        id: 1,
+        nome: "teste 1",
+        ativo: true,
+        valor: 100,
+        imagem: "celular.jpg",
+      },
+      {
+        id: 2,
+        nome: "teste 2",
+        ativo: true,
+        valor: 200,
+        imagem: "gopro.jpg",
+      },
+      {
+        id: 3,
+        nome: "teste 3",
+        ativo: true,
+        valor: 400,
+        imagem: "mouse.jpg",
+      },
+      {
+        id: 4,
+        nome: "teste 4",
+        ativo: true,
+        valor: 100,
+        imagem: "celular.jpg",
+      },
+      {
+        id: 5,
+        nome: "teste 5",
+        ativo: true,
+        valor: 500,
+        imagem: "teclado.jpg",
+      },
+      {
+        id: 6,
+        nome: "teste 5",
+        ativo: false,
+        valor: 300,
+        imagem: "headset.jpg",
+      },
+    ];
+  }
+
 
 }
