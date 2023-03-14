18c05cf3ef094ddfb54bde9fd2322942e1af799e
Carlos Eduardo <carlospn200406@gmail.com>
2023-03-13 21:15:53 -0300

    feat: add imports

--- PrimeiroProjetoAngular/src/app/demos/arquitetura-componentes/produto.module.ts
+++ PrimeiroProjetoAngular/src/app/demos/arquitetura-componentes/produto.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from "@angular/common";
 import { ProdutoDashboardComponent } from "./produto-dashboard/produto-dashboard.component";
 import { RouterModule } from "@angular/router";
 import { ProdutoRouterConfig } from "./produto.router";
+import { registerLocaleData } from "@angular/common";
+import localePt from "@angular/common/locales/pt"
+registerLocaleData(localePt)
 
 
 
