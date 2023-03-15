7f983aeb4d4981e6957a9b461e645153a08f8e14
Carlos Eduardo <carlospn200406@gmail.com>
2023-03-15 17:05:21 -0300

    feat: add nav vetor

--- PrimeiroProjetoAngular/src/app/navegacao/menu/menu.component.ts
+++ PrimeiroProjetoAngular/src/app/navegacao/menu/menu.component.ts
@@ -8,4 +8,51 @@ import { Component } from '@angular/core';
 })
 export class MenuComponent {
 
+  nav: Nav[] = [
+    {
+      link: '/home',
+      name: 'Home',
+      exact: true,
+      admin: false
+    },
+    {
+      link: '/cadastro',
+      name: 'Cadastro',
+      exact: true,
+      admin: false
+    },
+    {
+      link: '/sobre',
+      name: 'Sobre',
+      exact: true,
+      admin: false
+    },
+    {
+      link: '/produtos',
+      name: 'Produtos',
+      exact: true,
+      admin: false
+    },
+    {
+      link: '/contato',
+      name: 'Contato',
+      exact: true,
+      admin: false
+    },
+    {
+      link: '/produtos',
+      name: 'Produtos',
+      exact: true,
+      admin: false
+    }
+  ]
+
 }
+
+interface Nav {
+    link: string,
+    name: string,
+    exact: boolean,
+    admin: boolean,
+}
+
