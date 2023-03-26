4bc1ecb03d21f334003fcc10211293cf8efb47cc
Carlos Eduardo <carlospn200406@gmail.com>
2023-03-26 16:20:17 -0300

    .

--- PrimeiroProjetoAngular/src/app/demos/pipes/filmes/image.pipe.ts
+++ PrimeiroProjetoAngular/src/app/demos/pipes/filmes/image.pipe.ts
@@ -1,24 +1,18 @@
-import { Pipe, PipeTransform } from "@angular/core"
-
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: 'imageformater'
+  name: "imageformater",
 })
-
 export class ImageFormartPipe implements PipeTransform {
-
-  transform(imagem: string, caminho: string = '', substituir: boolean) {
-      
-    if(caminho == 'default'){
-      caminho = 'assets'
+  transform(imagem: string, caminho: string = "", substituir: boolean) {
+    if (caminho == "default") {
+      caminho = "assets";
     }
 
-    if(imagem.length == 0 && substituir){
-      imagem = 'semCapa.jpg'
+    if (imagem.length == 0 && substituir) {
+      imagem = "semCapa.jpg";
     }
 
-    return "/" + caminho + "/" + imagem
-
+    return "/" + caminho + "/" + imagem;
   }
-
 }
